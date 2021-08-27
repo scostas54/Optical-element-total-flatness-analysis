fdcc7b9850f3f01ffe0fa97ed697c59809bbca9b
scostas54 <70905913+scostas54@users.noreply.github.com>
2021-08-27 13:08:41 +0200

    Update corte.m

--- corte.m
+++ corte.m
@@ -1,7 +1,6 @@
-function [d]=corte(AB, xmin, ymin, w, h, wave)%vamos a cortar un rectángulo de la imagen para hacer el ajuste
-%por mínimos cuadrado
+function [d]=corte(AB, xmin, ymin, w, h, wave) % Image cut to use in Least Squares adjustment
 AB = im2double(AB);
 r=[xmin ymin w h];
 I=imcrop(AB,r);
 %D = 2*pi*I/255 ; % la matriz D tiene en cada celda los valores de ?
-d = wave*I*5/0.8 ; % obtiene las distancias de separación entre espejos en nm, si la longitud de onda está en nm.
+d = wave*I*5/0.8 ; % Obtains the distances between mirrors in nm, if the wavelength is in nm.
