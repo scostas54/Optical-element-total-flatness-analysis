bd1133af8c95853fea770e178cbe8433d22dd044
scostas54 <70905913+scostas54@users.noreply.github.com>
2021-08-27 13:10:54 +0200

    Update PoliCoef.m

--- PoliCoef.m
+++ PoliCoef.m
@@ -1,9 +1,9 @@
-function [D] = PoliCoef(x, y, z) % función que realiza un ajuste polinomial y devuelve los coeficientes 
-[xData, yData, zData] = prepareSurfaceData( x, y, z ); % prepara las matrices x,y,z para su uso en la función fit
-ft = fittype( 'poly11' ); % se establece el tipo de ajuste, en este caso polinomial de grado 1
-M = fit( [xData, yData], zData, ft); % se realiza el ajuste
-values=coeffvalues(M); % se almacenan los valores de los coeficientes en un vector [p00 p10 p01]
-D=values(1,1)+values(1,2)*x+values(1,3)*y; % plano de ajuste polinomial
+function [D] = PoliCoef(x, y, z) % function that performs a polynomial fit and returns the coefficients 
+[xData, yData, zData] = prepareSurfaceData( x, y, z ); % Prepares the x, y, z matrices for use in the fit function
+ft = fittype( 'poly11' ); % the type of fit is established, in this case polynomial of degree 1
+M = fit( [xData, yData], zData, ft); 
+values=coeffvalues(M); % the values of the coefficients are stored in a vector [p00 p10 p01]
+D=values(1,1)+values(1,2)*x+values(1,3)*y; % polynomial fit plane
 
 
 
